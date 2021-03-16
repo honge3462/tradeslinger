2278bf44cb7b8d5511f74f0b8b12c5a2b39ef2f7
Ellie Hong <hongek9@gmail.com>
2021-03-16 10:02:28 -0400

    cleaned up code

--- src/App.tsx
+++ src/App.tsx
@@ -54,48 +54,20 @@ const useStyles = makeStyles(() => ({
 
 function App() {
   const classes = useStyles();
+  const fonts = [classes.blantickScript, classes.foreFarmers, classes.grestalScript, classes.helloHoney, classes.kaileyLatief, classes.lifeStyle, classes.octoberTwilight, classes.sunFlowers];
   const [text, setText] = useState('')
   
   return (
     <div className="App">
       <Typography variant="h3">Type text here to see example fonts</Typography>
       <TextField id="outlined-basic" onChange={e => setText(e.target.value)} fullWidth label={""} variant="outlined" placeholder={"type text here"} />
+      
+      { fonts.map((font, index) => (
       <div className={classes.textBox}>
-        <Typography variant="h4">Font #1 - </Typography>
-        <Typography variant="h2" className={classes.blantickScript}>{text}</Typography>
+        <Typography variant="h4">Font #{index + 1} - </Typography>
+        <Typography variant="h2" className={font}>{text}</Typography>
       </div>
-      <div className={classes.textBox}>
-        <Typography variant="h4">Font #2 -</Typography>
-        <Typography variant="h2" className={classes.foreFarmers}>{text}</Typography>
-      </div>
-      <div className={classes.textBox}>
-        <Typography variant="h4">Font #3 -</Typography>
-        <Typography variant="h2" className={classes.grestalScript}>{text}</Typography>
-      </div>
-      <div className={classes.textBox}>
-        <Typography variant="h4">Font #4 -</Typography>
-        <Typography variant="h2" className={classes.helloHoney}>{text}</Typography>
-      </div>
-      <div className={classes.textBox}>
-        <Typography variant="h4">Font #5 -</Typography>
-        <Typography variant="h2" className={classes.kaileyLatief}>{text}</Typography>
-      </div>
-      <div className={classes.textBox}>
-        <Typography variant="h4">Font #6 -</Typography>
-        <Typography variant="h2" className={classes.lifeStyle}>{text}</Typography>
-      </div>
-      <div className={classes.textBox}>
-        <Typography variant="h4">Font #7 -</Typography>
-        <Typography variant="h2" className={classes.octoberTwilight}>{text}</Typography>
-      </div> 
-      <div className={classes.textBox}>
-        <Typography variant="h4">Font #8 -</Typography>
-        <Typography variant="h2" className={classes.sunFlowers}>{text}</Typography>
-      </div>  
-      <div className={classes.textBox}>
-        <Typography variant="h4">Font #9 -</Typography>
-        <Typography variant="h2" className={classes.sweetHipster}>{text}</Typography>
-      </div>
+      ))}
     </div>
   );
 }
